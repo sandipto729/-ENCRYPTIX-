ec3d90cb0f9198a4b1b6c2e743b78ec6c98f2eb9
Sandipto Roy <sandipto729@gmail.com>
2024-06-11 19:43:43 +0530

    Game 1 added

---
+++ game1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <cstdlib> 
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the maximum range of the number : ";
+    cin>>n;
+    const int number=rand()%n+1;
+    cout<<"Your game starts now : ";
+    int guess;
+    do
+    {
+        cin>>guess;
+        if(guess<number)
+            cout<<"Too low, Try again : ";
+        else if(guess>number)
+            cout<<"Too high, Try again : ";
+    } while (guess!=number);
+    cout<<"Correct!";
+    return 0;
+}
